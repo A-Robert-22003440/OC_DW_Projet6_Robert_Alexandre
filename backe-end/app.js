2b37321cc0a01a926bec073947481e9bb9a00a10
Alexandre Robert <alexandre.robert.22003440@gmail.com>
2026-05-01 12:58:19 +0200

    P3

--- backe-end/app.js
+++ backe-end/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://alexandrerobert22003440_db_user:<db_password>@cluster0.hphlibh.mongodb.net/?appName=Cluster0',
   { useNewUrlParser: true,
@@ -21,5 +22,6 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
